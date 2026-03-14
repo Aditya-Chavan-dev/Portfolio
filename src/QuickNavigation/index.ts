673788b31a9a6dfe045132acf342ebc20cce4997
Aditya Chavan <adityagchavan3@gmail.com>
2026-03-14 10:30:37 +0530

    chore: major refactor cleanup & AI rules setup

--- src/QuickNavigation/index.ts
+++
@@ -1,4 +0,0 @@
-export { AboutMe } from './AboutMe/AboutMe';
-export { Project } from './Project/Project';
-export { ProfessionalExperience } from './ProfessionalExperience/ProfessionalExperience';
-export { Certifications } from './Certifications/Certifications';
